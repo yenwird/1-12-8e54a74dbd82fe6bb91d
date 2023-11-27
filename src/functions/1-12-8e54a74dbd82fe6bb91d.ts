9aa18189e376eb5148e4f73cc7f155c62bcb8e9d
Yen Colon <yenjcc@gmail.com>
2023-11-27 18:13:56 -0400

    Flow Creation

---
+++ src/functions/1-12-8e54a74dbd82fe6bb91d.ts
@@ -0,0 +1,19 @@
+import { app, InvocationContext } from "@azure/functions";
+import * as https from "https";
+import * as df from 'durable-functions';
+import { ActivityHandler, OrchestrationContext, OrchestrationHandler } from 'durable-functions';
+
+const main => () => {}
+
+export async function serviceBusQueueTrigger(message: unknown, context: InvocationContext): Promise<void> {
+    context.log('Service bus queue function processed message:', message);
+    const client = df.getClient(context);
+    const instanceId: string = await client.startNew("1-12-8e54a74dbd82fe6bb91d", message);
+    context.log(`Started orchestration with ID = '${instanceId}'.`);
+}
+app.serviceBusQueue('1-12-8e54a74dbd82fe6bb91d', {
+    connection: 'azureQueueConnection',
+    queueName: '1-12-8e54a74dbd82fe6bb91d',
+    handler: serviceBusQueueTrigger,
+    extraInputs: [df.input.durableClient()],
+});

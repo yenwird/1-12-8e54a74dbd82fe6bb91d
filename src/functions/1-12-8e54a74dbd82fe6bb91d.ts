366c58fd0b75bb754436ba8a8e7ad0830795607e
yenwird <147008695+yenwird@users.noreply.github.com>
2023-11-27 18:18:51 -0400

    Update 1-12-8e54a74dbd82fe6bb91d.ts

--- src/functions/1-12-8e54a74dbd82fe6bb91d.ts
+++ src/functions/1-12-8e54a74dbd82fe6bb91d.ts
@@ -3,7 +3,9 @@ import * as https from "https";
 import * as df from 'durable-functions';
 import { ActivityHandler, OrchestrationContext, OrchestrationHandler } from 'durable-functions';
 
-const main => () => {};
+const main => () => {
+    
+};
 
 export async function serviceBusQueueTrigger(message: unknown, context: InvocationContext): Promise<void> {
     context.log('Service bus queue function processed message:', message);
